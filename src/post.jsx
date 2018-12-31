1dab0ab09f1d35709fd82b49b5ab8a8acb3decd1
jmn <norlund@gmail.com>
2018-12-31 16:35:38 +0100

    fix titles/seo

--- src/post.jsx
+++ src/post.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { graphql } from "gatsby";
+import SEO from "./components/seo";
 
 export default ({ data }) => {
   const post = data.markdownRemark;
   return (
     <div style={{ margin: `1rem auto`, maxWidth: 600 }}>
+      <SEO title={post.frontmatter.title} keywords={post.frontmatter.tags} />
       <h1>{post.frontmatter.title}</h1>
       <h4 style={{ color: "rgb(165, 164, 164)" }}>
         <span style={{ fontSize: "0.8em" }}>{post.fields.date}</span>
